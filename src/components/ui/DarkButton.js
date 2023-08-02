641738dab3b2e76332042bc55a6e57c602e0f2d2
Tilak Dave <davetilak003@gmail.com>
2023-08-02 12:37:09 +0530

    button font semibold

--- src/components/ui/DarkButton.js
+++ src/components/ui/DarkButton.js
@@ -1,6 +1,6 @@
 export default function DarkButton({ children }) {
     return (
-        <button className="bg-[#131313] text-[#D8B25D] px-12 py-3">
+        <button className="bg-[#131313] text-[#D8B25D] font-semibold px-12 py-3">
             {children}
         </button>
     );
