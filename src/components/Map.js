6f046be1cccee3f9c78c4c5474a9e0da0722eea3
Isha Bule <ishabule753@gmail.com>
2023-08-23 16:00:00 +0530

    List added

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,10 +1,4 @@
-import {
-    Dropdown,
-    Ripple,
-    initTE,
-} from "tw-elements";
-
-initTE({ Dropdown, Ripple });
+import React, { useState } from 'react';
 export default function Map() {
     var List = {
         "SHOPPING": [
@@ -62,7 +56,18 @@ export default function Map() {
             "Sector 65"
         ]
 
+
     }
+    const [expandedCategory, setExpandedCategory] = useState('');
+
+    const handleCategoryToggle = (category) => {
+        if (expandedCategory === category) {
+            setExpandedCategory('');
+        } else {
+            setExpandedCategory(category);
+        }
+    };
+
     return (
         <div className="flex flex-col mt-2 md:px-10 lg:px-20">
             <h2 className="font-hl text-[18px] text-left md:text-3xl my-2 md:my-0 px-6 md:px-0">
@@ -73,57 +78,29 @@ export default function Map() {
                 <img src="/images/map.png" alt="map" className="md:w-3/5 md:mr-8" />
 
                 <div className="flex flex-col w-full px-6 md:px-0 mt-6 md:mt-0">
-                    {Object.keys(List).map((category, index) => (
-                        <div key={index} className="relative" data-te-dropdown-ref>
-                            <Dropdown
-                                data-te-dropdown-toggle-ref
-                                id={`dropdownMenuButton${index}`}
-                                trigger={() => (
-                                    <button
-                                        className="flex items-center whitespace-nowrap rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] motion-reduce:transition-none dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
-                                        type="button"
-                                        aria-expanded="false"
-                                        data-te-ripple-init
-                                        data-te-ripple-color="light"
-                                    >
-                                        {category}
-                                        <span className="ml-2 w-2">
-                                            <svg
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                viewBox="0 0 20 20"
-                                                fill="currentColor"
-                                                className="h-5 w-5"
-                                            >
-                                                <path
-                                                    fillRule="evenodd"
-                                                    d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
-                                                    clipRule="evenodd"
-                                                />
-                                            </svg>
-                                        </span>
-                                    </button>
-                                )}
+                    {Object.keys(List).map((category) => (
+                        <div key={category} className="mb-2">
+                            <button
+                                className="border-t-2 border-[#D8B25D] px-6 py-3 flex justify-between items-center w-full"
+                                onClick={() => handleCategoryToggle(category)}
                             >
-                                <ul
-                                    className="absolute z-[1000] float-left m-0 hidden min-w-max list-none overflow-hidden rounded-lg border-none bg-white bg-clip-padding text-left text-base shadow-lg dark:bg-neutral-700 [&[data-te-dropdown-show]]:block"
-                                    aria-labelledby={`dropdownMenuButton${index}`}
-                                    data-te-dropdown-menu-ref
-                                >
-                                    {List[category].map((item, itemIndex) => (
-                                        <li key={itemIndex}>
-                                            <a
-                                                className="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-neutral-600"
-                                                href="#"
-                                                data-te-dropdown-item-ref
-                                            >
-                                                {item}
-                                            </a>
-                                        </li>
+                                <p className="font-hl uppercase md:text-[18px] text-[13px]">{category}</p>
+                                <img src="/images/down.svg" alt="arrow" className="w-6" />
+                            </button>
+                            {expandedCategory === category && (
+                                <ul className="mt-2">
+                                    {List[category].map((landmark) => (
+                                        <li key={landmark} className='font uppercase border-t-2 border-[#faecce] px-6 py-3 flex'>{landmark}</li>
                                     ))}
                                 </ul>
-                            </Dropdown>
+                            )}
                         </div>
                     ))}
+
+
+
+
+
                 </div>
             </div>
         </div>
